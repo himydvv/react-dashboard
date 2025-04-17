496e052de5f727c399bef5acaefcba67b17b6c06
himydvv <himyadav8989@gmail.com>
2025-04-18 00:07:00 +0530

    first commit

---
+++ src/Mainpage/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+ const Mainpage=()=>{
+    
+   
+      const Card = {
+        
+      };
+    
+      return (<div class="flex flex-col items-center justify-center h-screen dark">
+        <div class="w-full max-w-md bg-gray-800 rounded-lg shadow-md p-6">
+          <h2 class="text-2xl font-bold text-gray-200 mb-4">Login</h2>
+          
+            </div>
+</div>
+      )
+    };
+    
+    export default Mainpage;
+    
+   
