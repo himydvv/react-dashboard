a8c0767a0d2f512d5084644f3d5f3cea992fcf21
himydvv <himyadav8989@gmail.com>
2025-04-18 16:55:09 +0530

    setup of react router dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Mainpage from './Mainpage';
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
 function App() {
   return (
-   <Routes>
    
-   <Route path="/" element={<Mainpage />}>
-       
+  //  <Routes>
+   
+  //  <Route path="/" element={<Mainpage />}>
+  //  <Route index element={<Navigate to="/" replace />} />
         
-      </Route>
-   </Routes>
-
+  //     </Route>
+  //  </Routes>
+<div><h1>hi guys</h1></div>
   
 
 
